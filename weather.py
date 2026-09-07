e345b92de91b1f8fc9acc5c857a2648021cb77e6
Sk Akmam Uddin <skakmamuddinrohan@gmail.com>
2026-09-07 20:04:14 +0530

    Include weather forecast data in response
    Added forecast data including dates, rain, and temperature ranges.

--- weather.py
+++ weather.py
@@ -24,10 +24,15 @@ def get_weather(latitude, longitude):
         current = data["current"]
 
         return {
-            "temperature": current["temperature_2m"],
-            "humidity": current["relative_humidity_2m"],
-            "rain": current["rain"],
-            "wind_speed": current["wind_speed_10m"]
+    "temperature": current["temperature_2m"],
+    "humidity": current["relative_humidity_2m"],
+    "rain": current["rain"],
+    "wind_speed": current["wind_speed_10m"],
+
+    "forecast_dates": data["daily"]["time"],
+    "forecast_rain": data["daily"]["precipitation_sum"],
+    "forecast_temp_max": data["daily"]["temperature_2m_max"],
+    "forecast_temp_min": data["daily"]["temperature_2m_min"]
         }
 
     except Exception as e:
